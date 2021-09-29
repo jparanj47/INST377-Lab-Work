e90acabb58eabe7aa93aba63d6f0a2af7de35bb2
jparanj47 <89950550+jparanj47@users.noreply.github.com>
2021-09-29 12:08:18 -0400

    upward platform count

--- client/flappy1/app.js
+++ client/flappy1/app.js
@@ -6,6 +6,7 @@ document.addEventListener('DOMContentLoaded', () =>{
     let birdLeft = 220
     let birdBottom = 100 
     let gravity = 2
+    let isGameOver = false
 
     function startGame(){
 
@@ -14,12 +15,51 @@ document.addEventListener('DOMContentLoaded', () =>{
         bird.style.left = birdLeft + 'px'
     
     }       
-    let timerId = setInterval(startGame, 20)
+    let gameTimerId = setInterval(startGame, 20)
 
+    function control(e) {
+        if (e.keyCode === 32){
+            jump()
+        }
+    }
 
     function jump() {
-        birdBottom += 50
+        if (birdBottom < 500) birdBottom += 50
         bird.style.bottom = birdBottom + 'px'
+        console.log(birdBottom)
     }
-    document.addEventListener('keyup', jump)
+    document.addEventListener('keyup', control)
+
+    function generateObstacle(){
+       let obstacleLeft = 500 
+       let randomHeight = Math.random() * 60
+       let obstacleBottom = randomHeight
+       const obstacle = document.createElement('div')
+       obstacle.classList.add('obstacle')
+       gameDisplay.appendChild(obstacle)
+       obstacle.style.left = obstacleLeft + 'px'
+       //obstacle.style.bottom = obstacleBottom + 'px'
+
+    //    function moveObstacle(){
+    //        obstacleLeft -=2
+    //        obstacle.style.left = obstacleLeft + 'px'
+
+    //        if(obstacleLeft === -60){
+    //            clearInterval()
+    //            gameDisplay.removeChild(obstacle)
+    //        }
+    //        if(birdBottom === 0){
+    //            gameOver()
+    //        }
+    //    }
+    //    let timerId = setInterval(moveObstacle, 20)
+    //    setTimeout(generateObstacle, 3000)
+    }
+    generateObstacle()
+
+    // function gameOver(){
+    //     clearInterval(gameTimerId)
+    //     isGameOver = true
+    //     document.removeEventListener('keyup', control)
+    // }
 })
